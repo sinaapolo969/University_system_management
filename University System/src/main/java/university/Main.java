ff2e16e86bacf0ebef6396565d40c6b4e6745c5b
Sina Tayebi <sinaapolo969@gmail.com>
2022-04-22 00:34:22 +0430

    edit student info impelemented!

--- University System/src/main/java/university/Main.java
+++ University System/src/main/java/university/Main.java
@@ -74,10 +74,6 @@ public class Main
         return faculty1.createStudentAccount(name, facultyName, department);
     }
 
-    public static void addCourse(Student student)
-    {
-        student.addCourse(student);
-    }
 
     public static Professor ProfessorSignIn()
     {
@@ -144,7 +140,6 @@ public class Main
             System.err.println("Invalid username(ID)! please try again!");
             professorLogin();
         }
-
         return null;
     }
 
@@ -173,23 +168,21 @@ public class Main
                 catch (Exception error)
                 {
                     System.out.println("there is not any faculty yet! please make a faculty and try again!");
-                    System.out.println("press any key for come back to manager dashboard!");
+                    System.out.println("press any key for back to dashboard");
                     String input = in.next();
-                    if (!input.isEmpty())
-                    {
-                        ManagerDashboard();
-                    }
-                    else
+                    if (input.isEmpty())
                     {
                         ManagerDashboard();
                     }
                 }
-
                 break;
             default:
-                System.out.println("wrong input! \n please choose a number from the list");
-                ManagerDashboard();
-
+                System.out.println("press any key for back to dashboard");
+                String input = in.next();
+                if (input.isEmpty())
+                {
+                    ManagerDashboard();
+                }
         }
         menu();
     }
@@ -207,7 +200,7 @@ public class Main
         else if (number2.equals("2"))
         {
             Professor professor = professorLogin();
-            System.out.println("name:  " + professor.getProfessorName() + "  " + "ID:  " +
+            System.out.println("name:  " + professor.getProfessorName() + "\nID:  " +
                     professor.getProfessorID());
             System.out.println();
             System.out.println("1- set score \n2- information \n3- edit info\n4- back to menu");
@@ -228,10 +221,26 @@ public class Main
             {
                 menu();
             }
+            else
+            {
+                String input = messagePrinter();
+                if (input.isEmpty())
+                {
+                    ProfessorDashboard();
+                }
+            }
         }
         else if (number2.equals("3"))
         {
             menu();
+        }
+        else
+        {
+            String input = messagePrinter();
+            if (input.isEmpty())
+            {
+                ProfessorDashboard();
+            }
         }
         menu();
     }
@@ -260,60 +269,21 @@ public class Main
             }
             else if (number3.equals("2"))
             {
-                System.out.println("1- personal information \n2- term info");
+                System.out.println("1- personal information \n2- term info and average");
                 System.out.println("please enter the number: ");
                 String command = in.next();
                 switch (command)
                 {
                     case "1":
-                        System.out.println("name: " + student.getName()
-                                + "ID: " + student.getID() + "\n"
-                                + "Faculty " + student.getFaculty()
-                                + "Department: " + student.getDepartment() +
-                                "Entrance year: " + student.getEntranceYear());
-                        System.out.println("press any key for back to student dashboard");
-                        String innerCommand = in.next();
-                        if (!innerCommand.isEmpty())
-                        {
-                            StudentDashboard();
-                        }
+                        displayStudentPersonalInfo(student);
                         break;
                     case "2":
-                        System.out.println("1- class schedule \n2- term average");
-                        System.out.println("please enter the number: ");
-                        String innerCommand2 = in.next();
-                        switch (innerCommand2)
-                        {
-                            case "1":
-                                System.out.println("list of courses: ");
-                                int i = 1;
-                                for (Course course: student.courses)
-                                {
-                                    System.out.println(i + "_" +"course ID:  " + course.getCourseID() +
-                                            "  course name:  " + course.getCourseName()
-                                            + "  course unit:  " +
-                                            course.getUnit());
-                                    i++;
-                                }
-                                break;
-                            case "2":
-                                Faculty faculty = FacultyInfo.get(0);
-                                for (String name : student.scores.keySet())
-                                {
-                                    System.out.println("course name:  " + faculty.courseInfo.get(name)
-                                            + "  score: " + student.scores.get(name));
-                                }
-                                System.out.println("Average: " + student.getTermAvg());
-                                break;
-                            default:
-                                throw new IllegalArgumentException("wrong input!");
-                        }
-
+                        displayStudentAcademicalInfo(student);
                 }
             }
             else if (number3.equals("3"))
             {
-
+                editStudentInfo(student);
                 menu();
             }
             else if (number3.equals("4"))
@@ -322,7 +292,11 @@ public class Main
             }
             else
             {
-                throw new IllegalArgumentException("wrong input!");
+                String input = messagePrinter();
+                if (input.isEmpty())
+                {
+                    StudentDashboard();
+                }
             }
         }
         else if (number2.equals("3"))
@@ -331,8 +305,11 @@ public class Main
         }
         else
         {
-            throw new IllegalArgumentException("wrong input!");
-
+            String input = messagePrinter();
+            if (input.isEmpty())
+            {
+                StudentDashboard();
+            }
         }
         menu();
     }
@@ -340,16 +317,103 @@ public class Main
     public static void displayProfessorInfo(Professor professor)
     {
         System.out.println("<<PROFESSOR INFORMATION DASHBOARD>>");
-        System.out.println("name:  " + professor.getProfessorName() + "\n ID:  " + professor.getProfessorID()
-        + "\n faculty name:  " + professor.getFacultyName() + "\n department:  " + professor.getDepartment());
+        System.out.println("name:  " + professor.getProfessorName() + "\nID:  " + professor.getProfessorID()
+        + "\nfaculty name:  " + professor.getFacultyName() + "\ndepartment:  " + professor.getDepartment());
         System.out.println();
         System.out.println("<<LIST OF CLASSES>>");
         int i = 1;
         for (Classroom classroom : professor.classes)
         {
-            System.out.println(i + "class name:  " + classroom.getCourse().getCourseName()
-                    + "  class ID: " + classroom.getClassID());
+            System.out.println(i + "- class name:  " + classroom.getCourse().getCourseName()
+                    + "\nclass ID: " + classroom.getClassID());
         }
+        System.out.println("\n");
+        messagePrinter();
+    }
+
+    public static void displayStudentPersonalInfo(Student student)
+    {
+        System.out.println("<<STUDENT PERSONAL INFORMATION DASHBOARD>>");
+        System.out.println("name:  " + student.getName() + "\nID:  " + student.getID()
+                + "\n entrance year: " + student.getEntranceYear() + "\nfaculty name:  " + student.getFaculty()
+                + "\n department: " + student.getDepartment());
+        String input = messagePrinter();
+        if (input.isEmpty())
+        {
+            StudentDashboard();
+        }
+    }
+
+    public static void displayStudentAcademicalInfo(Student student)
+    {
+        System.out.println("<<COURSE INFORMATION DASHBOARD>>");
+        int i = 1;
+        for (Course course: student.courses)
+        {
+            System.out.println(i + "_ " + "course name:  " + course.getCourseName() + "  course ID:  " +
+                    course.getCourseID() + "  course unit:  " + course.getUnit());
+            i++;
+        }
+        System.out.println("\n");
+        System.out.println("<<TERM INFORMATION AND AVERAGE>>");
+        Faculty faculty = FacultyInfo.get(0);
+        for (String name : student.scores.keySet())
+        {
+            System.out.println("course name:  " + faculty.courseInfo.get(name)
+                    + "  score: " + student.scores.get(name));
+        }
+        System.out.println("Average: " + student.getTermAvg());
+        String input = messagePrinter();
+        if (input.isEmpty())
+        {
+            StudentDashboard();
+        }
+
+    }
+
+    public static void editStudentInfo(Student student)
+    {
+        displayStudentPersonalInfo(student);
+        System.out.println("\n");
+        System.out.println("<<EDIT STUDENT INFORMATION DASHBOARD>>");
+        System.out.println();
+        System.out.println("please enter your name: ");
+        String name = in.next();
+        System.out.println("faculties: ");
+        int i = 1;
+        for (Faculty faculty : AmouzeshInfo.get(0).facultyInfo.values())
+        {
+            System.out.println(i + "- " + "faculty name:  " + faculty.getFacultyName() +
+                    "  faculty ID:  " + faculty.facultyID);
+            i++;
+        }
+        System.out.println("please enter the faculty ID: ");
+        String facultyId = in.next();
+        String facultyName;
+        if (AmouzeshInfo.get(0).facultyInfo.containsKey(facultyId))
+        {
+            facultyName = AmouzeshInfo.get(0).facultyInfo.get(facultyId).getFacultyName();
+        }
+        else
+        {
+            facultyName = null;
+            System.out.println("this faculty hasnt initialize yet! \n please choose the ID from the list!");
+            studentSignIn();
+        }
+        System.out.println("please enter your department: ");
+        String department = in.next();
+        Faculty faculty1 = FacultyInfo.get(0);
+        student.setName(name);
+        student.setFaculty(facultyName);
+        student.setDepartment(department);
+        faculty1.studentInfo.replace(student.getID(), student);
+        System.out.println("information was successfully edited!");
+        String input = messagePrinter();
+        if (input.isEmpty())
+        {
+            StudentDashboard();
+        }
+
     }
 
     public static void editProfessorInfo(Professor professor)
@@ -388,12 +452,18 @@ public class Main
         professor.setDepartment(department);
         faculty1.professorInfo.replace(professor.getProfessorID(), professor);
         System.out.println("information was successfully edited!");
-        System.out.println("press any key for back to professor dashboard");
-        String input = in.next();
-        if (!input.isEmpty())
+        String input = messagePrinter();
+        if (input.isEmpty())
         {
             ProfessorDashboard();
         }
+    }
+    
+    public static String messagePrinter()
+    {
+        System.out.println("press any key for back to dashboard");
+        String input = in.next();
+        return input;
     }
 
 }
